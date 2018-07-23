c942bd9a9dfa79eb51aaa523c9c7dc83def62995
Steven Albert <steven.a.540@hotmail.com>
2018-07-23 10:43:45 +0700

    Remove unused files

--- function.R
+++
@@ -1,9 +0,0 @@
-#Function to check and install the package
-requirePackage <- function(x) {
-  if(!require(x, character.only = TRUE)) {
-    install.packages(x)
-    if(!require(x, character.only = TRUE)) stop("Package not found")
-  }
-  library(x, character.only = TRUE)
-}
-
